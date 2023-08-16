3d921c1343c792b553f525642aa0c976953a7f74
Matt <matthew.ellis@cyberhive.com>
2023-08-16 13:13:31 +0100

    Renamed defaults/main.yml to example and added defaults/main.yml to git ignore.

--- molecule/default/tests/test_role.py
+++ molecule/default/tests/test_role.py
@@ -1,5 +1,13 @@
 import pytest
 
+import os
+
+import testinfra.utils.ansible_runner
+
+testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
+    os.environ["MOLECULE_INVENTORY_FILE"]
+).get_hosts("all")
+
 
 @pytest.mark.parametrize(
     "name",
